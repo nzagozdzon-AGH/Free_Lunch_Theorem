a39d3e5640f712636b9de6b0dc978bc6bfcf815d
nzagozdzon-AGH <nzagozdzon@student.agh.edu.pl>
2025-12-08 16:58:23 +0100

    I made some basic functions and analized Admiralbet offer

---
+++ engine.py
@@ -0,0 +1,137 @@
+# Imports
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def overround_calculator(list_of_odds: list) -> int:
+    """
+    It calculates bookmaker overround for given betting odds. 
+    """
+    avg_overround = 0
+    for odds in list_of_odds:
+        p1 = 1/odds[0]
+        p2 = 1/odds[1]
+        # If more than two odds where given for function
+        if len(odds) == 3:
+            p3 = 1/odds[2]
+        else:
+            p3 = 0
+        overround = p1+p2+p3
+        avg_overround += overround
+    avg_overround /= len(list_of_odds)
+    return avg_overround
+
+def one_bet(odds: float, stake: float, overround: float, legs: int, tax: bool) -> float:
+    """
+    Simulates one bet and returns winnings if won.
+    """
+    overround = overround**legs
+    p_win = 1/odds/overround
+
+    if random.random() < p_win:
+        if tax:
+            return stake*0.88*odds # adjusted for tax
+        else:
+            return stake*odds
+    else:
+        return 0
+    
+def many_bets(odds: float, stake: float, overround: float, legs: int, tax: bool, money: int, req_turnover: int, free_bet: dict | None) -> float:
+    """
+    Simulates turnover of the bonus
+    """
+    turnover = 0
+    if free_bet is not None:
+        money += bet_free_bet(free_bet, overround, tax)
+    while turnover < req_turnover:
+        money -= stake
+        turnover += stake
+        money += one_bet(odds, stake, overround, legs, tax)
+        if money <= 0:
+            break
+    return money
+
+def monte_carlo(offer: dict, overround: float) -> np.ndarray:
+    """
+    Performs Monte Carlo simulation of given betting strategy
+    """
+    odds = offer["min_odds"] + 0.15 # I add a small value, cause it's hard to find offer with exatcly minimal odds
+    stake = 10 # For any offer I reccomend betting 10 PLN in each bet for diversification
+    legs = offer["legs"]
+    money = offer["bonus"] + offer["deposit"]
+    req_turnover = offer["turnover"]
+    tax = offer["tax"]
+    free_bet = offer["free_bet"]
+
+    results = []
+    tries = 10000
+    while tries > 0:
+        tries -= 1
+        results.append(many_bets(odds, stake, overround, legs, tax, money, req_turnover, free_bet))
+    results = np.array(results) - offer["deposit"]
+    return results
+
+
+def visualize_results(results: np.ndarray, title: str) -> None:
+    """
+    Displays histogram to present data from monte carlo simulation.
+    """
+    # Calculating p_win and of loss
+    loss_count = np.sum(results <= 0)
+    win_count = np.sum(results > 0)
+    total = len(results)
+    p_loss = (loss_count / total) * 100
+    p_win = (win_count / total) * 100
+    
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(results, bins=26, color="#0A2F80CF", edgecolor='black')
+    plt.axvline(0, color="#a80000", linewidth=5, linestyle='--')
+    
+    y_min, y_max = plt.ylim()
+    x_min, x_max = plt.xlim()
+    
+    # Tekst po lewej (Strata) - umieszczony w 25% szerokości lewej strony
+    plt.text(x_min * 0.7, y_max * 0.9, 
+             f"Chance of loss:\n{p_loss:.1f}%", 
+             color='#a80000', fontsize=12, fontweight='bold', ha='center')
+
+
+    text_pos_x = max(x_max * 0.5, x_max * 0.1) if x_max > 0 else 0
+    
+    plt.text(text_pos_x, y_max * 0.9, 
+             f"Chance of profit:\n{p_win:.1f}%", 
+             color='#27ae60', fontsize=12, fontweight='bold', ha='center')
+
+
+    plt.title(title, fontsize=14, fontweight='bold')
+    plt.xlabel('Net Result (PLN)', fontsize=12)
+    plt.ylabel('Number of simulations', fontsize=12)
+    plt.grid(True, linestyle=':', alpha=0.6)
+    plt.legend()
+    
+    plt.show()
+
+def confidence_interval(results: np.ndarray) -> str:
+    lower = np.percentile(results, 20)
+    upper = np.percentile(results, 80)
+    return f"60% confidence interval: [{lower:.2f}, {upper:.2f}]"
+
+
+def bet_free_bet(free_bet_dict: dict, overround: float, tax: bool) -> float:
+    stake = free_bet_dict["value"]
+    odds = free_bet_dict["odds"]
+    overround = overround**free_bet_dict["legs"]
+    p_win = 1/odds/overround
+
+    if random.random() < p_win:
+        if free_bet_dict["SR"] and tax:
+            return stake*0.88*odds
+        elif not free_bet_dict["SR"] and tax:
+            return stake*0.88*odds - stake
+        elif free_bet_dict["SR"] and not tax:
+            return stake*odds
+        else:
+            return stake*odds - stake
+    else:
+        return 0
---
+++ offers.py
@@ -0,0 +1,39 @@
+Admiralbet = {
+    "Offer_1": {
+        "turnover": 600,
+        "bonus": 200,
+        "deposit": 200,
+        "legs": 3,
+        "min_odds": 2.5,
+        "tax": True,
+        "free_bet": {
+            "value": 25,
+            "odds": 3.5, # it's SNR, so I recommend playing with bigger odds
+            "SR": False,
+            "legs": 1
+        }
+    },
+    "Offer_2": {
+        "turnover": 1800,
+        "bonus": 600,
+        "deposit": 1200,
+        "legs": 3,
+        "min_odds": 2.5,
+        "tax": True,
+        "free_bet": {
+            "value": 75,
+            "odds": 3.5, # it's SNR, so I recommend playing with bigger odds
+            "SR": False,
+            "legs": 1
+        }
+    },
+    "Offer_3": {
+        "turnover": 1600,
+        "bonus": 400,
+        "deposit": 1600,
+        "legs": 3,
+        "min_odds": 2.5,
+        "tax": True,
+        "free_bet": None
+    }
+}
